24edeca940b54948db7f35b33f6847f10cce5235
unknown <zhtmr123@gmail.com>
2021-02-14 18:41:28 +0900

    xml추가

---
+++ src/main/java/hellojpa/JpaMain.java
@@ -0,0 +1,9 @@
+package hellojpa;
+
+import javax.persistence.Persistence;
+
+public class JpaMain {
+    public static void main(String[] args) {
+        Persistence.createEntityManagerFactory()
+    }
+}
